be890be044ef47ff767f5c73a6819d8ff1012d4b
Charmaine I. Cura <cicura@headoffice.corp.bpi.com.ph>
2025-12-03 17:31:44 +0800

    1st M1_Activity5

---
+++ Activity5.java
@@ -0,0 +1,17 @@
+package ph.com.bpi.hello;
+import java.util.Scanner;
+
+public class Activity5 {
+
+	public static void main(String[] args) {
+		//TODO Auto-generated method stub
+		int sum =0;
+		for (int i = 0; i < 51; i++) {
+			
+			sum =(i + sum);
+			
+			}
+               
+		System.out.println("Sum:" + sum);
+}
+}
