1a67184aa22e0370298cdd3b8ed47285733a2948
Charmaine I. Cura <cicura@headoffice.corp.bpi.com.ph>
2025-12-03 17:18:26 +0800

    1st commit for M1_Activity1

---
+++ Activity1.java
@@ -0,0 +1,15 @@
+package ph.com.bpi.hello;
+import java.util.Scanner;
+
+public class Activity1 {
+	public static void main(String[] args) {
+		//TODO Auto-generated method stub
+       Scanner input = new Scanner(System.in);
+	System.out.println("What is your name?");
+          String name = input.nextLine();
+         System.out.println("Hello " + name);
+          
+    
+	}
+}
+
