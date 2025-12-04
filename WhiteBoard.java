00f2321ee4915f072165a7dd75c443ce7e7c1930
Charmaine I. Cura <cicura@headoffice.corp.bpi.com.ph>
2025-12-04 18:55:33 +0800

    1st M2_Activity2

---
+++ WhiteBoard.java
@@ -0,0 +1,10 @@
+package ph.com.bpi.hello;
+
+public class WhiteBoard {
+  int sum = 0;	
+  
+  int addNumbers(int addend1,int addend2) {
+	  this.sum = addend1 + addend2;
+	 return this.sum; 
+  }
+}
