c708bde8b81da0e840cebc1d7b48127a402370d8
Charmaine I. Cura <cicura@headoffice.corp.bpi.com.ph>
2026-02-03 17:10:28 +0800

    1st commit M6_Activity2

---
+++ M6/src/test/java/App.java
@@ -0,0 +1,37 @@
+import util.EntityManagerUtil;
+import jakarta.persistence.EntityManager;
+import model.Student;
+
+public class App {
+	
+	public static void main(String[] args) {
+		EntityManager em = EntityManagerUtil.getInstance().createEntityManager();
+
+try {
+			runM6Activity2(em);
+		} finally {
+			EntityManagerUtil.getInstance().closeEntityManager(em);
+			EntityManagerUtil.getInstance().shutdownFactory();
+		}
+	}
+
+	static void runM6Activity2(EntityManager em) {
+		
+		try {
+			em.getTransaction().begin();
+
+			Student newStudent = new Student();
+			newStudent.setName("Juan Dela Cruz");
+			newStudent.setAge(50);
+			newStudent.setEmail("juandelacruz@gmail.com");
+
+			em.persist(newStudent);
+			em.getTransaction().commit();
+		} 
+		finally {
+			
+		}
+
+	}
+}
+
